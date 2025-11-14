b2ad45432202b58569d4af4c243ae35f2329db1d
Samara Oliveira <163425297+10mara@users.noreply.github.com>
2025-11-14 10:07:49 -0300

    Criação da tela Criar Conta
    Criação da tela de criar conta

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import InicioSessao from './pages/InicioSessao/InicioSessao.jsx';
 import NotFound from './pages/NotFound.jsx';
+import TelaInicial from './pages/TelaInicial.jsx'
+import CriarConta from './pages/CriarConta.jsx'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sessao" element={<InicioSessao />} />
+        <Route path="/inicio" element={<TelaInicial />} />
+        <Route path="/criarconta" element={<CriarConta/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

1c7c4e81dd148428f0ff20a27b4dae1eeaf02577
marjorygc <163425550+marjorygc@users.noreply.github.com>
2025-11-24 10:02:06 -0300

    Correção de erros
    foram corrigidos os erros na conexão da tabela question e option, agora ambas estão funcionais permitindo a plena utilização dessa parte do banco de dados

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import CreateQuiz from './pages/CreateQuiz';
+import './pages/CreateQuiz/CreateQuiz'
 
 import { useState } from 'react'
 
